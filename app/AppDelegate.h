6f9a2975e379a8f4df52e93a693781b851497307
Nagmani Tiwari <Nagmani@Krishna.local>
2017-05-01 17:19:04 +0530

    Initial Commit

---
+++ app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app
+//
+//  Created by Nagmani Tiwari on 5/1/17.
+//  Copyright © 2017 Dotzoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  app
+//
+//  Created by Nagmani Tiwari on 5/1/17.
+//  Copyright © 2017 Dotzoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
